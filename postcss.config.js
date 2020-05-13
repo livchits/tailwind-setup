966aa921bffd1da7d921d19dbaa192d51c101c3b
Lucas Livchits <livchitsl@gmail.com>
2020-05-13 20:43:49 -0300

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano')({ preset: 'default' });
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/*.html'],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+process.env.NODE_ENV === 'production'
+  ? (module.exports = {
+      plugins: [tailwindcss, autoprefixer, purgecss, cssnano],
+    })
+  : (module.exports = {
+      plugins: [tailwindcss, purgecss],
+    });
